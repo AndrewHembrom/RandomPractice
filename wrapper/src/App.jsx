33577d93650234cdbd8b37deae4ac35e71bb00c4
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-07-27 10:58:56 +0530

    understanding wrappers

--- wrapper/src/App.jsx
+++ wrapper/src/App.jsx
@@ -3,8 +3,23 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div>
-      Hi There!!      
+    <div style={{display: "flex"}}>
+      <CardWrapper>
+        Some text
+      </CardWrapper>
+      <CardWrapper>
+        <div>
+          some more text
+        </div>
+      </CardWrapper>
+    </div>
+  )
+}
+
+function CardWrapper({ children }) {
+  return (
+    <div style={{ border: "1px solid black", padding: 20, margin: 20 }}>
+      {children}
     </div>
   )
 }
