60ffd3a7e811b709164b2f50cb0628a82d249442
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-07-28 20:36:19 +0530

    useMemo completed

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect, useMemo} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = useMemo(() => {
+    let finalCount = 0;
+    for (let i = 0; i <= inputValue; i++) {
+      finalCount = finalCount + i;
+    }
+    return finalCount;
+  }, [inputValue]);
+
+  // const [finalValue, setFinalValue] = useState(1);
+
+  // useEffect(() => { 
+  //   let count = 0;
+  //   for (let i = 0; i <= inputValue; i++) { 
+  //     count = count + i;
+  //   }
+  //   setFinalValue(count);
+  // },[inputValue])
+
+  // let count = 0;
+  // for (let i = 0; i <= inputValue; i++) { 
+  //   count = count + i;
+  // }
 
   return (
     <div>
-
+      <input onChange={function (e) { 
+        setInputValue(e.target.value);
+      }} placeholder='Find sum from 1 to n' />
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
+      <button onClick={() => { 
+        setCounter(counter+1)
+      }}>Counter ({counter})</button>
     </div>
   )
 }
