fe2815b325c7695b5e99b01eeaa74f201a188f3b
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-08 22:02:07 +0530

    context-api counter self

---
+++ context-api/src/App.jsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from 'react'
+import { CountContext } from "./context";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count />
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count() { 
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  )
+}
+
+function CountRenderer() {
+  const { count } = useContext(CountContext);
+
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+function Buttons() { 
+  const { count, setCount } = useContext(CountContext);
+
+  return (
+    <div>
+      <button onClick={() => { setCount(count - 1) }}>Decrease</button>
+      <button onClick={() => { setCount(count + 1) }}>Increase</button>
+    </div>
+  )
+}
+
+export default App
