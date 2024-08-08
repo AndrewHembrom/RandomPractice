7350cdecccf354ee2108fcde34c670b03d86d26a
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-08 22:24:42 +0530

    recoil'

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,14 +1,15 @@
 import { useContext, useState } from 'react'
 import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { countAtom } from './store/atoms/count';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <CountContext.Provider value={{ count, setCount }}>
+      <RecoilRoot>
         <Count />
-      </CountContext.Provider>
+      </RecoilRoot>
     </div>
   )
 }
@@ -23,7 +24,7 @@ function Count() {
 }
 
 function CountRenderer() {
-  const { count } = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
 
   return (
     <div>
@@ -33,12 +34,12 @@ function CountRenderer() {
 }
 
 function Buttons() { 
-  const { count, setCount } = useContext(CountContext);
+  const setCount = useSetRecoilState(countAtom);
 
   return (
     <div>
-      <button onClick={() => { setCount(count - 1) }}>Decrease</button>
-      <button onClick={() => { setCount(count + 1) }}>Increase</button>
+      <button onClick={() => { setCount(count => count - 1) }}>Decrease</button>
+      <button onClick={() => { setCount(count => count + 1) }}>Increase</button>
     </div>
   )
 }
