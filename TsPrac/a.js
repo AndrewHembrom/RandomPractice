01d178a7b57869d55de1f9a158c97be2c042e3f6
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-08-12 22:25:12 +0530

    types in typeScript

--- TsPrac/a.js
+++ TsPrac/a.js
@@ -1,12 +1,8 @@
-
+"use strict";
 // TypeScript adds type safety to JavaScript code
 // let x: number = 121;
 // console.log(x);
-
-
-// Types - number, string, boolean, null, undefined, any
-function greet(firstName: string) { 
+function greet(firstName) {
     console.log(`Hello ${firstName}`);
 }
-
 greet("Andrew");
